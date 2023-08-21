95c0114d1bf6eb4153d8423e6a41a217ff076706
kimjaehoon <kjh950805@naver.com>
2023-08-21 13:12:46 +0900

    0821-2-gender add

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   // 변수 return위에 변수
   const name = "싹난감자";
   let age = 28 ;
+  let gender = true ;
+  // 객체
   const naver = {
     name : "네이버",
     url : "https://www.naver.com",
